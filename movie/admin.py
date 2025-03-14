7aee144c7d4236a7deb72950d71ef120cd476343
mohammad charipour <mohammd.ch81m@gmail.com>
2025-03-14 11:25:37 +0330

    register models in admin panel

--- movie/admin.py
+++ movie/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import ActorAndDirector, Content, Genre, Rating, Recommendation, Role, Season, Series, Episode, Watchlist
 
-# Register your models here.
+
+admin.site.register(ActorAndDirector)
+admin.site.register(Content)
+admin.site.register(Genre)
+admin.site.register(Rating)
+admin.site.register(Role)
+admin.site.register(Recommendation)
+admin.site.register(Series)
+admin.site.register(Season)
+admin.site.register(Episode)
+admin.site.register(Watchlist)
+
--- movie/models/__init__.py
+++ movie/models/__init__.py
@@ -5,5 +5,6 @@ from .role import Role
 from .series import Series, Season, Episode
 from .watchlist import Watchlist
 from .recommendation import Recommendation
+from .rating import Rating
 
-__all__ = ['Genre', 'ActorAndDirector', 'Content', 'Role', 'Series', 'Season', 'Episode', 'Watchlist', 'Recommendation']
+__all__ = ['Genre', 'ActorAndDirector', 'Content', 'Role', 'Series', 'Season', 'Episode', 'Watchlist', 'Recommendation', 'Rating']
